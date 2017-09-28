afe2a34c098d01bb586731ddb390a80b1c923cb9
vailalgatt <vail.algatt@gmail.com>
2017-09-28 10:24:26 -0600

    testing angular forms out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'app';
 }
+  
+  // (function(angular) {
+  //   'use strict';
+  //   angular.module('formExample', [])
+  //   .controller('ExampleController', ['$scope', function($scope) {
+  //     $scope.master = {};
+      
+  //     $scope.update = function(user) {
+  //       $scope.master = angular.copy(user);
+  //     };
+      
+  //     $scope.reset = function(form) {
+  //       if (form) {
+  //         form.$setPristine();
+  //         form.$setUntouched();
+  //       }
+  //       $scope.user = angular.copy($scope.master);
+  //     };
+      
+  //     $scope.reset();
+  //   }]);
+  // })(window.angular);
+  
